b5bb37634f6232b453f8834d2ed982a46a4f2ee0
JnsCas <jnss.cas@gmail.com>
2013-06-04 21:23:43 -0300

    posible diseño

---
+++ manejodestock1/src/dominio/ProductoFinal.groovy
@@ -0,0 +1,51 @@
+package dominio
+
+class ProductoFinal {
+	
+	// atributos:
+	//			minimaDeStock
+	//			maximaDeStock
+	//			cantDePedido
+	//			estado (simple o compuesto, si es compuesto cargar la lista de componentes)
+	//			List<Observador> observadores
+	//
+	// public bajoStockMinimo(Stock unStock)
+	//	{
+	//		return cantidad de producto dentro de unStock < minimaDeStock
+	//  }
+	//
+	// public cantDePedido()
+	//  {
+	//		return cantDePedido
+	//  }
+	//	
+	// public salida(Stock unStock) {
+	//	
+	//		habria que registrar en un contador la cantidad de veces que se le hizo
+	//      remove en un determinado stock, o algo asi.
+	// }
+	//
+	// public excedeStockMaximo(Stock unStock) {
+	//
+	//	return cantidad de producto dentro de unStock > maximaDeStock
+	// 
+	// }
+	//
+	// public notificar(Stock unStock)
+	//	{
+	//    para todo observador en observadores 
+	//				{
+	//					observador.actualizar(this, unStock)
+	//				}
+	//  }
+	//
+	// public adscribir(Observador unObservador) {
+	//	meter en observadores
+	//
+	// }
+	//
+	// public quitar(Observador unObservador) {
+	// sacar de observadores
+	//
+	// }
+}
